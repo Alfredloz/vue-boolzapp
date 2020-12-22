2802a4294281f8617eec16083da37d949a769f8a
Alfredo <alfredogregorio.lozano@gmail.com>
2020-12-22 17:28:28 +0100

    milestones 4 completata
    manca migliroamenti grafici

--- milestone4/assets/js/main.js
+++ milestone4/assets/js/main.js
@@ -1,11 +1,11 @@
-// ● Aggiunta di un messaggio​: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// ● Risposta dall’interlocutore: ​ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// ● Ricerca utenti: ​scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 let app = new Vue({
     el: '#app',
     data:{
 
         messageUser:'',
         activeUser: 0,
+        searchContact: '',
         messageBot:{
             date:'data da scegliere',
             text: 'ok',
@@ -179,5 +179,12 @@ let app = new Vue({
             
         },
         
-    }
+    },
+    computed:{
+        filteredItems(){
+            return this.contacts.filter((item)=>{
+                return item.name.toLowerCase().includes(this.searchContact.toLowerCase());
+            });
+        }
+    },
 })
